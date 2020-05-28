22c6e4bb75413830a3c20e5c0bd2628f476764e8
sujeongim0912 <sujeongim0912@users.noreply.github.com>
2020-05-28 20:43:11 +0900

    audio+laugh

--- audio_integ.py
+++ audio_integ.py
@@ -22,6 +22,7 @@ class AudioClass:
     def __init__(self, duration):
         self.duration = duration
         self.word_lst = []
+        self.laugh_time = []
 
 # [audio segment class - text, st, et, db, freq]
 class WordClass:
@@ -98,8 +99,7 @@ def sample_long_running_recognize(storage_uri):
         alternative = result.alternatives[0]
         print(u"Transcript: {}".format(alternative.transcript))
         # Print the start and end time of each word
-        st = 0
-        et = 0
+
         for word in alternative.words:
             st = word.start_time.seconds * 1000 + word.start_time.nanos * 0.000001
             et = word.end_time.seconds*1000 + word.end_time.nanos * 0.000001
@@ -125,9 +125,7 @@ def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
-def audio_analyze_word(out_file, word_list, sr):
-    audio = AudioSegment.from_wav(out_file)
-
+def audio_analyze_word(audio, word_list):
     for w in word_list:
         word_audio = audio[w.start_time:w.end_time + 1]
         w.dbfs = word_audio.dBFS
@@ -159,9 +157,48 @@ def audio_analyze_word(out_file, word_list, sr):
     return word_list
 
 
+def find_laughter(seg, audio_seg, word_list):
+    j = 0
+    laugh_st = 0
+    laugh_et = 0
+    is_laugh = False
+    dB = 0
+    for i in range(len(audio_seg)-1):
+        """if word_list[j].start_time <= i <= word_list[j].end_time:
+            is_laugh = False
+            continue"""
+        """else: 
+            if i > word_list[j].end_time:
+                j += 1"""
+        #print(i)
+        if audio_seg[i].max_dBFS >= -17:  # laugh false
+            if is_laugh == False:
+                is_laugh = True
+                laugh_st = i
+                laugh_et = i
+            else:
+                laugh_et += 1
+            dB = audio_seg[i].max_dBFS
+
+        elif audio_seg[i].max_dBFS < -17:
+            if is_laugh == True:
+                if laugh_et - laugh_st >= 800:
+                   seg.laugh_time.append((dB, laugh_st, laugh_et))
+                is_laugh = False
+    print(len(seg.laugh_time))
+    for i in range(len(seg.laugh_time)):
+        print(seg.laugh_time[i])
+
+
 def audio_analyze_sliced(out_file, i):
     y, sr = librosa.load(out_file)
+    audio = AudioSegment.from_wav(out_file)
+    #print(audio.rms)
     duration = librosa.get_duration(y=y, sr=sr) * 1000
+
+    #print(len(audio))
+    #print("duration", duration)
+
     global total_duration
     total_duration += duration
     seg = AudioClass(duration)
@@ -179,8 +216,8 @@ def audio_analyze_sliced(out_file, i):
     word_list = sample_long_running_recognize(args.storage_uri)
 
     #analyze words - dB
-    seg.word_lst = audio_analyze_word(out_file, word_list, sr)
-    print(seg.word_lst)
+    seg.word_lst = audio_analyze_word(audio, word_list)
+
     """for w in word_list:
         print("text: ", w.text)
         print("start_time: ", w.start_time)
@@ -190,7 +227,10 @@ def audio_analyze_sliced(out_file, i):
         print("freq_end: ", w.freq_e)
         print()
     """
+    # 웃음소리 찾기
+    find_laughter(seg, audio, seg.word_lst)
     return seg
+
 
 def audio_analysis_main(inputfile_name):
     #video_to_audio
@@ -199,7 +239,8 @@ def audio_analysis_main(inputfile_name):
 
     # audio_chunk
     sound_file = AudioSegment.from_wav("audio.wav")
-    audio_chunks = split_on_silence(sound_file, min_silence_len=200, silence_thresh=-20, keep_silence=300) # oh hi hi hi -> 400, -30, 400
+    dBFS = sound_file.dBFS
+    audio_chunks = split_on_silence(sound_file, min_silence_len=500, silence_thresh=-30, keep_silence=300) # oh hi hi hi -> 400, -30, 400
 
     audio_list = []
 
@@ -216,3 +257,6 @@ def audio_analysis_main(inputfile_name):
     global total_duration
     print(total_duration)
     return audio_list
+
+if __name__ == "__main__":
+    audio_analysis_main('input_video/bigbang.mp4')
